63d523ace402e165f5b7300555974b6651268d2d
yoshi314 <yoshi314@gmail.com>
2011-11-29 12:46:39 +0100

    cleaned up some configs. slim is not available for 12.1 atm, so a copy is provided.
    also added default session for tux users. X doesn't come up automatically, figuring it out.

--- config/config.sh
+++ config/config.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-#================
-# FILE          : config.sh
-#----------------
-# PROJECT       : OpenSuSE KIWI Image System
-# COPYRIGHT     : (c) 2006 SUSE LINUX Products GmbH. All rights reserved
-#               :
-# AUTHOR        : Marcus Schaefer <ms@suse.de>
-#               :
-# BELONGS TO    : Operating System images
-#               :
-# DESCRIPTION   : configuration script for SUSE based
-#               : operating systems
-#               :
-#               :
-# STATUS        : BETA
-#----------------
-#======================================
-# Functions...
-#--------------------------------------
-test -f /.kconfig && . /.kconfig
-test -f /.profile && . /.profile
 
 #======================================
 # Greeting...
@@ -35,44 +14,23 @@ suseConfig
 echo "** Running ldconfig..."
 /sbin/ldconfig
 
-sed --in-place -e 's/icewm/icewm-session/' /usr/bin/wmlist
+chkconfig earlyxdm on
+chkconfig xdm on
 
 #======================================
-# Firewall Configuration
 #--------------------------------------
-echo '** Configuring firewall...'
-chkconfig SuSEfirewall2_init on
-chkconfig SuSEfirewall2_setup on
+baseRepo="http://download.opensuse.org/distribution/12.1/repo/oss"
+baseName="suse-12.1-oss"
+zypper ar $baseRepo $baseName
 
 #======================================
-# RPM GPG Keys Configuration
+# Remove unneeded packages
 #--------------------------------------
-echo '** Importing GPG Keys...'
-rpm --import /studio/studio_rpm_key_0
-rm /studio/studio_rpm_key_0
-rpm --import /studio/studio_rpm_key_1
-rm /studio/studio_rpm_key_1
-rpm --import /studio/studio_rpm_key_2
-rm /studio/studio_rpm_key_2
-rpm --import /studio/studio_rpm_key_3
-rm /studio/studio_rpm_key_3
-rpm --import /studio/studio_rpm_key_4
-rm /studio/studio_rpm_key_4
-rpm --import /studio/studio_rpm_key_5
-rm /studio/studio_rpm_key_5
-rpm --import /studio/studio_rpm_key_6
-rm /studio/studio_rpm_key_6
-rpm --import /studio/studio_rpm_key_7
-rm /studio/studio_rpm_key_7
-rpm --import /studio/studio_rpm_key_8
-rm /studio/studio_rpm_key_8
+rpm -qa | grep yast | xargs rpm -e --nodeps
 
-sed --in-place -e 's/# solver.onlyRequires.*/solver.onlyRequires = true/' /etc/zypp/zypp.conf
+#======================================
+# Umount kernel filesystems
+#--------------------------------------
+baseCleanMount
 
-# Enable sshd
-chkconfig sshd on
-test -d /studio || mkdir /studio
-cp /image/.profile /studio/profile
-cp /image/config.xml /studio/config.xml
-rm -rf /studio/overlay-tmp
-true
+exit 0
---
+++ config/images.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+echo "Configure image: [$kiwi_iname]..."
+
+#==========================================
+# remove unneeded packages
+#------------------------------------------
+rpm -e --nodeps --noscripts \
+	$(rpm -q `baseGetPackagesForDeletion` | grep -v "is not installed")
+
+#==========================================
+# umount /proc
+#------------------------------------------
+umount /proc &>/dev/null
+
+exit 0
