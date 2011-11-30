688959ad48c8f756ab02e327be961c6848dc72a6
root <root@linux-7i04.(none)>
2011-11-30 17:26:11 +0100

    rewritten config.sh based on live opensuse media configuration.
    login manager now works, network manager also works. nm-applet fails to connect to it, though atm.

--- config/config.sh
+++ config/config.sh
@@ -1,43 +1,67 @@
 #!/bin/bash
-
+#================
+# FILE          : config.sh
+#----------------
+# PROJECT       : OpenSuSE KIWI Image System
+# COPYRIGHT     : (c) 2006,2007,2008 SUSE LINUX Products GmbH. All rights reserved
+#               :
+# AUTHOR        : Marcus Schaefer <ms@suse.de>, Stephan Kulow <coolo@suse.de>
+#               :
+# LICENSE       : BSD
+#======================================
+# Functions...
+#--------------------------------------
 test -f /.kconfig && . /.kconfig
 test -f /.profile && . /.profile
+
+set -e
+
+exec | tee /var/log/config.log
+exec 2>&1
 
 #======================================
 # Greeting...
 #--------------------------------------
 echo "Configure image: [$name]..."
 
+chkconfig boot.ldconfig off
+
+#======================================
+# /etc/sudoers hack to fix #297695 
+# (Installation Live CD: no need to ask for password of root)
+#--------------------------------------
+sed -i -e "s/ALL ALL=(ALL) ALL/ALL ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers 
+chmod 0440 /etc/sudoers
+
+# delete passwords
+passwd -d root
+passwd -d tux
+# empty password is ok
+pam-config -a --nullok
+
+: > /var/log/zypper.log
+
+mv /var/lib/livecd/*.pdf /home/linux || true
+rmdir /var/lib/livecd || true
+
 #======================================
 # SuSEconfig
 #--------------------------------------
-echo "** Running suseConfig..."
 suseConfig
+SuSEconfig --module permissions
 
-echo "** Running ldconfig..."
-/sbin/ldconfig
+rm -rf /var/cache/zypp/packages
 
-echo "** setting up services **"
+# bug 544314
+sed -i -e 's,^\(.*pam_gnome_keyring.so.*\),#\1,'  /etc/pam.d/common-auth-pc
 
+echo '127.0.0.2       linux.site linux' >> /etc/hosts
 
-#stip unnecessary libs
-baseStripUnusedLibs
+baseSetRunlevel 5
+baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER_AUTOLOGIN tux
+baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER slim
+baseUpdateSysConfig /etc/sysconfig/keyboard KEYTABLE us.map.gz
+baseUpdateSysConfig /etc/sysconfig/keyboard YAST_KEYBOARD english-us,pc104
+baseUpdateSysConfig /etc/sysconfig/clock HWCLOCK "-u"
 
-
-#======================================
-#--------------------------------------
-baseRepo="http://download.opensuse.org/distribution/12.1/repo/oss"
-baseName="suse-12.1-oss"
-zypper ar $baseRepo $baseName
-
-#======================================
-# Remove unneeded packages
-#--------------------------------------
-rpm -qa | grep yast | xargs rpm -e --nodeps
-
-#======================================
-# Umount kernel filesystems
-#--------------------------------------
-baseCleanMount
-
-exit 0
+baseUpdateSysConfig /etc/sysconfig/network/config NETWORKMANAGER yes
