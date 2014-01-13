8764b907be92577a710b3259173b7abdd6238d80
yoshi314 <yoshi314@gmail.com>
2014-01-13 11:57:43 +0100

    update to 13.1, lightdm, various changes

--- config/config.sh
+++ config/config.sh
@@ -3,63 +3,96 @@
 # FILE          : config.sh
 #----------------
 # PROJECT       : OpenSuSE KIWI Image System
-# COPYRIGHT     : (c) 2006,2007,2008 SUSE LINUX Products GmbH. All rights reserved
+# COPYRIGHT     : (c) 2006 SUSE LINUX Products GmbH. All rights reserved
 #               :
-# AUTHOR        : Marcus Schaefer <ms@suse.de>, Stephan Kulow <coolo@suse.de>
+# AUTHOR        : Marcus Schaefer <ms@suse.de>
 #               :
-# LICENSE       : BSD
+# BELONGS TO    : Operating System images
+#               :
+# DESCRIPTION   : configuration script for SUSE based
+#               : operating systems
+#               :
+#               :
+# STATUS        : BETA
+#----------------
 #======================================
 # Functions...
 #--------------------------------------
 test -f /.kconfig && . /.kconfig
 test -f /.profile && . /.profile
 
-set -e
-
-exec | tee /var/log/config.log
-exec 2>&1
-
 #======================================
 # Greeting...
 #--------------------------------------
-echo "Configure image: [$name]..."
-
-chkconfig boot.ldconfig off
+echo "Configure image: [$kiwi_iname]..."
 
 #======================================
-# /etc/sudoers hack to fix #297695 
-# (Installation Live CD: no need to ask for password of root)
+# Mount system filesystems
 #--------------------------------------
-sed -i -e "s/ALL ALL=(ALL) ALL/ALL ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers 
-chmod 0440 /etc/sudoers
+baseMount
 
-# delete passwords
-passwd -d root
-passwd -d tux
-# empty password is ok
-pam-config -a --nullok
+#======================================
+# Setup baseproduct link
+#--------------------------------------
+suseSetupProduct
 
-: > /var/log/zypper.log
+#======================================
+# Add missing gpg keys to rpm
+#--------------------------------------
+suseImportBuildKey
 
-mv /var/lib/livecd/*.pdf /home/linux || true
-rmdir /var/lib/livecd || true
+#======================================
+# Activate services
+#--------------------------------------
+suseInsertService sshd
+
+#======================================
+# Setup default target, multi-user
+#--------------------------------------
+baseSetRunlevel 5
+
+#==========================================
+# remove package docs
+#------------------------------------------
+rm -rf /usr/share/doc/packages/*
+rm -rf /usr/share/doc/manual/*
+rm -rf /opt/kde*
+
+#======================================
+# only basic version of vim is
+# installed; no syntax highlighting
+#--------------------------------------
+sed -i -e's/^syntax on/" syntax on/' /etc/vimrc
 
 #======================================
 # SuSEconfig
 #--------------------------------------
 suseConfig
 
-rm -rf /var/cache/zypp/packages
+baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER lightdm
+baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER_REMOTE_ACCESS yes
+baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER_STARTS_XSERVER yes
+baseUpdateSysConfig /etc/sysconfig/windowmanager DEFAULT_WM i3
+baseUpdateSysConfig /etc/sysconfig/language RC_LANG pl_PL.UTF-8
 
-# bug 544314
-sed -i -e 's,^\(.*pam_gnome_keyring.so.*\),#\1,'  /etc/pam.d/common-auth-pc
 
-baseSetRunlevel 5
-baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER_AUTOLOGIN tux
-baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER gdm
-baseUpdateSysConfig /etc/sysconfig/keyboard KEYTABLE us.map.gz
-baseUpdateSysConfig /etc/sysconfig/keyboard YAST_KEYBOARD english-us,pc104
-baseUpdateSysConfig /etc/sysconfig/clock HWCLOCK "-u"
+systemctl enable NetworkManager.service
 
-baseUpdateSysConfig /etc/sysconfig/network/config NETWORKMANAGER yes
+#======================================
+# Add 13.2 repo
+#--------------------------------------
+baseRepo="http://download.opensuse.org/distribution/13.2/repo/oss"
+baseName="suse-13.2"
+zypper ar $baseRepo $baseName
 
+#======================================
+# Remove yast packages
+#--------------------------------------
+rpm -qa | grep yast | xargs rpm -e --nodeps
+
+#======================================
+# Umount kernel filesystems
+#--------------------------------------
+baseCleanMount
+
+exit 0
